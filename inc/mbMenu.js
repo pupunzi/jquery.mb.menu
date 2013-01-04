39dba65262d2b4786b7af51fb33ce3e9b1b30596
pupunzi <matteo@open-lab.com>
2013-01-04 17:35:17 +0100

    various

--- inc/mbMenu.js
+++ inc/mbMenu.js
@@ -1,23 +1,23 @@
 /*
  * ******************************************************************************
  *  jquery.mb.components
+ *  file: mbMenu.js
  *
- *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi); Open lab srl, Firenze - Italy
+ *  Copyright (c) 2001-2013. Matteo Bicocchi (Pupunzi);
+ *  Open lab srl, Firenze - Italy
  *  email: matteo@open-lab.com
- *  site: http://pupunzi.com
+ *  site: 	http://pupunzi.com
+ *  blog:	http://pupunzi.open-lab.com
+ * 	http://open-lab.com
  *
  *  Licences: MIT, GPL
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
+ *
+ *  last modified: 04/01/13 17.13
  *  *****************************************************************************
  */
 
-/*
- * Name:jquery.mb.menu
- * Version: 2.8.9rc5
- *
- * added: boxMenu menu modality by: Sven Dowideit http://trunk.fosiki.com/Sandbox/WebSubMenu
- */
 // to get the element that is fireing a contextMenu event you have $.mbMenu.lastContextMenuEl that returns an object.
 
 (function($) {
