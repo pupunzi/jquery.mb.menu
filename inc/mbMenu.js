f88291aa4e862791f6358ab9c6f03e543ad51ee0
pupunzi <matteo@open-lab.com>
2013-04-29 22:10:21 +0200

    various fix

--- inc/mbMenu.js
+++ inc/mbMenu.js
@@ -14,7 +14,7 @@
  *  http://www.opensource.org/licenses/mit-license.php
  *  http://www.gnu.org/licenses/gpl.html
  *
- *  last modified: 03/02/13 22.47
+ *  last modified: 25/04/13 22.58
  *  *****************************************************************************
  */
 
